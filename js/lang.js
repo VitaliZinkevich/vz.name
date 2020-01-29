d006a0d3e26df9afc9a59df0b10538f462d6e68e
Vitali Zinkevich <vitalizinkevich@gmail.com>
2020-01-29 22:08:23 +0300

    грамматика

--- js/lang.js
+++ js/lang.js
@@ -32,7 +32,7 @@ var translateO = {
   "Desing and Develepment Mobile and Web apps.":
     "Разработка и дизайн мобилиных и веб приложений",
   "Maintenance and Support.": "Техническое обслуживание и поддержка",
-  "IT Consultancy.": "Консультации по проэкту",
+  "IT Consultancy.": "Консультации по проекту",
   "CHECK IT OUT": "ПРОВЕРЬТЕ САМИ",
   "Iterative development": "Разработка итерациями",
   "Our clients receive daily reports of the work completed, as well as working demo versions of the project. Due to this, you can make necessary adjustments at any stage, resulting in a product of the highest quality.":
@@ -42,15 +42,15 @@ var translateO = {
     "Постоянный контакт с клиентом позволяет создать доверительную и и удобную среду для сотрудничества и избежать многих недоразумений",
   "Fixed-time cycle": "Фиксированные временные этапы",
   "For easy control of the workflow and to minimize possible bugs, the whole project is divided into fixed time periods. At the end of each iteration the working product is compiled and analyzed and possible errors are identified. At the same time, a plan for further action is formulated.":
-    "Для простого контроля рабочего процесса и минимизации возможных багов в рабочем приложении, весь проэкт разделен на фиксированные временные этапы. В конце каждого периода проэкт собирается в приложение для тестирования и анализа. В тоже время дополняется или актуализируется план на следующий временной этап.",
+    "Для простого контроля рабочего процесса и минимизации возможных багов в рабочем приложении, весь проект разделен на фиксированные временные этапы. В конце каждого периода проект собирается в приложение для тестирования и анализа. В тоже время дополняется или актуализируется план на следующий временной этап.",
   Testing: "Тесты",
   "Continuous integration helps to reduce the complexity of integration and make it more reliable, due to early detection and correction of errors and inconsistencies.":
-    "Благодаря постоянному конторолю рабочей демо версии приложения снижается сложность внедрения, делая его более предсказуемым. Благодаря постоянному анализу и поиску багов в приложении.",
+    "Благодаря постоянному контролю рабочей демо версии приложения снижается сложность внедрения, делая его более предсказуемым. Благодаря постоянному анализу и поиску багов в приложении.",
   "— Unit Testing — Integration testing — Continuous integration":
     "Используется юнит тестирование кода, тестирование при внедрении, тестирование при изменениях.",
   Contact: "Контакты",
   "Ready to start your next project? Give a call or send an email and We will get back to you as soon as possible!":
-    "Готовы начать свой проэкт? Свяжитесь с нами по телефону, мессенджеру, напишите в соц сети или по почте.",
+    "Готовы начать свой проект? Свяжитесь с нами по телефону, мессенджеру, напишите в соц сети или по почте.",
   "I am at Facebook": "Я на Фейсбуке"
 };
 
